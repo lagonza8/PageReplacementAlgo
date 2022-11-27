b88482bd7ae028b4be7146edd43a692e62daf2b2
lagonza5 <lagonza5@asu.edu>
2022-11-27 14:36:11 -0700

    Add DataLoader.c file for definitions

---
+++ DataLoader.c
@@ -0,0 +1,29 @@
+/**
+ * Definitions for helper functions to load a reference string.
+ *
+ * @author Luis A. Gonzalez, Acuna
+ * @version 0.9
+ */
+
+
+//structs
+/*
+struct test_scenario {
+    int refstr_len;
+    int refstr[512];
+    int page_count;
+    int frame_count;
+};
+*/
+
+
+/**
+ * Loads a test_scenario strut from a textfile.
+ *
+ * @param filename The name of the file to load.
+ * @return A struct containing the loaded file.
+ */
+struct test_scenario* load_test_data(char* filename)
+{
+
+}
