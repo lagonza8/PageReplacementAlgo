f8e1fa199c3482b64e6e8ec5a9638377704d05cb
lagonza5 <lagonza5@asu.edu>
2022-11-28 19:30:13 -0700

    Replace for loop with while loop for populating array

--- DataLoader.c
+++ DataLoader.c
@@ -55,7 +55,8 @@ struct test_scenario* load_test_data(char* filename)
     printf("reference string entry count: %d \n", data->refstr_len);
 
     // The rest of the file is dedicated to the page numbers in the reference string
-    for (int i = 0; i < data->refstr_len; i++) {
+    int i;
+    while (!feof(filePointer)) {
         data->refstr[i] = strtol(fgets(buffer, SIZE, filePointer), &ptr, 10);
         printf("%d \n", data->refstr[i]);
     }
